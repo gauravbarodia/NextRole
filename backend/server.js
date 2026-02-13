fbf00c8888b21176733319fdd8b8e3ec4c3a1cf9
gauravbarodia <gaurav.barodia.min22@itbhu.ac.in>
2026-02-14 00:02:39 +0530

    host_1.1

--- backend/server.js
+++ backend/server.js
@@ -112,5 +112,7 @@ app.delete('/jobs/:id', async (req, res) => {
   }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
