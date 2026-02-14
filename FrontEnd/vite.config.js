85af4c670693e0cce1195f188c5a47ec64bea562
gauravbarodia <gaurav.barodia.min22@itbhu.ac.in>
2026-02-14 14:07:02 +0530

    Fix CORS and remove localhost

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],preview: {
     allowedHosts: [
-      "frontend-production-49cc.up.railway.app" 
+      "nextrole.up.railway.app" 
     ]
   }
 })
